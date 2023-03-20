63f4fe557946a15e5d698a5c8fb93a96fe6669dd
ajadaah <ajadaa@i3hub.com>
2023-03-20 11:43:50 +0200

    init commit

---
+++ src/main/java/org/i3hub/dialect/CustomMySQL8Dialect.java
@@ -0,0 +1,16 @@
+package org.i3hub.dialect;
+
+import com.vladmihalcea.hibernate.type.json.JsonStringType;
+import org.hibernate.dialect.MySQL8Dialect;
+
+import java.sql.Types;
+
+public class CustomMySQL8Dialect extends MySQL8Dialect {
+
+    public CustomMySQL8Dialect() {
+        super();
+        this.registerHibernateType(
+            Types.OTHER, JsonStringType.class.getName()
+        );
+    }
+}
